3f134ee82610dc5f150c7111989628cd7e82cf6c
bukalex <gurylevalexey@gmail.com>
2024-01-30 00:08:43 +0300

    Player does not stand up after death

--- Lord of Dungeons/Assets/Scripts/PlayerAnimationController.cs
+++ Lord of Dungeons/Assets/Scripts/PlayerAnimationController.cs
@@ -10,91 +10,97 @@ public class PlayerAnimationController : MonoBehaviour
     private KeyCode horizontalKeyCode = KeyCode.S;
     private bool interrupt = false;
     private PolygonCollider2D[] polygonColliders;
+    private GeneralCharacterController characterController;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         polygonColliders = transform.parent.GetComponents<PolygonCollider2D>();
+        characterController = transform.parent.GetComponent<GeneralCharacterController>();
+
         transform.localPosition = new Vector3(0, 0, 0);
     }
 
     void Update()
     {
-        //Change direction
-        if (Input.GetKeyDown(KeyCode.A))
+        if (characterController.isAlive())
         {
-            DisablePolygonColliders(2);
+            //Change direction
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                DisablePolygonColliders(2);
 
-            horizontalKeyCode = KeyCode.A;
-            animator.SetTrigger("left");
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            DisablePolygonColliders(3);
+                horizontalKeyCode = KeyCode.A;
+                animator.SetTrigger("left");
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                DisablePolygonColliders(3);
 
-            horizontalKeyCode = KeyCode.D;
-            animator.SetTrigger("right");
-        }
-        else if (Input.GetKeyDown(KeyCode.W))
-        {
-            DisablePolygonColliders(1);
+                horizontalKeyCode = KeyCode.D;
+                animator.SetTrigger("right");
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                DisablePolygonColliders(1);
 
-            verticalKeyCode = KeyCode.W;
-            animator.SetTrigger("back");
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            DisablePolygonColliders(0);
+                verticalKeyCode = KeyCode.W;
+                animator.SetTrigger("back");
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                DisablePolygonColliders(0);
 
-            verticalKeyCode = KeyCode.S;
-            animator.SetTrigger("front");
-        }
+                verticalKeyCode = KeyCode.S;
+                animator.SetTrigger("front");
+            }
 
-        //Start walking
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
-        {
-            animator.SetBool("isWalking", true);
-        }
+            //Start walking
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+            {
+                animator.SetBool("isWalking", true);
+            }
 
-        //Stop walking
-        if (!Input.GetKey(verticalKeyCode) && !Input.GetKey(horizontalKeyCode))
-        {
-            animator.SetBool("isWalking", false);
-        }
+            //Stop walking
+            if (!Input.GetKey(verticalKeyCode) && !Input.GetKey(horizontalKeyCode))
+            {
+                animator.SetBool("isWalking", false);
+            }
 
-        //Start runnign
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            animator.SetBool("isRunning", true);
-        }
+            //Start runnign
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                animator.SetBool("isRunning", true);
+            }
 
-        //Stop runnign
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            animator.SetBool("isRunning", false);
-        }
+            //Stop runnign
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                animator.SetBool("isRunning", false);
+            }
 
-        //Attack 1
-        if (Input.GetMouseButton(0) && !interrupt)
-        {
-            animator.SetBool("isRunning", false);
-            animator.SetBool("isWalking", false);
-            animator.SetTrigger("attack1");
-        }
+            //Attack 1
+            if (Input.GetMouseButton(0) && !interrupt)
+            {
+                animator.SetBool("isRunning", false);
+                animator.SetBool("isWalking", false);
+                animator.SetTrigger("attack1");
+            }
 
-        //Attack 2
-        if (Input.GetMouseButton(1) && !interrupt)
-        {
-            animator.SetBool("isRunning", false);
-            animator.SetBool("isWalking", false);
-            animator.SetTrigger("attack2");
-        }
+            //Attack 2
+            if (Input.GetMouseButton(1) && !interrupt)
+            {
+                animator.SetBool("isRunning", false);
+                animator.SetBool("isWalking", false);
+                animator.SetTrigger("attack2");
+            }
 
-        //Stop attacking
-        if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1) || interrupt)
-        {
-            animator.ResetTrigger("attack1");
-            animator.ResetTrigger("attack2");
+            //Stop attacking
+            if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1) || interrupt)
+            {
+                animator.ResetTrigger("attack1");
+                animator.ResetTrigger("attack2");
+            }
         }
     }
 
