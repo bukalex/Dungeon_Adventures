4f9983a3fd3c1524a81c8aafd36e79b9ee6acb6c
Aleksei Gurylev <buka_lex@mail.ru>
2024-01-30 18:15:08 +0300

    Update ItemParameters.cs

--- Lord of Dungeons/Assets/ScriptableObjects/Scripts/ItemParameters.cs
+++ Lord of Dungeons/Assets/ScriptableObjects/Scripts/ItemParameters.cs
@@ -8,6 +8,11 @@ public class ItemParameters : ScriptableObject
     public PlayerData playerData;
 
     //Stats
+    public float damageMult;
+    public float healthMult;
+    public int price;
+    public bool isRecyclable;
+    public bool isSellable;
 
 
     //Image
@@ -19,10 +24,13 @@ public class ItemParameters : ScriptableObject
     //Types
     public ItemCategory category = ItemCategory.RESOURCES;
     public ResourceType resourceType = ResourceType.COIN;
-    public EquipmentType equipmentType;
+    public EquipmentType equipmentType ;
+    public itemClass items;
 
     //Enums
     public enum ItemCategory { RESOURCES, EQUIPMENT }
     public enum ResourceType { COIN, WOOD, ROCK }
     public enum EquipmentType { SWORD }
+
+    public enum itemClass { OFF, A, B, C, D, E}
 }
