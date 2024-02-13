582807db5b48a7bbda7a11477d29ff1aad23d175
Kuikon <shibuhayato@gmail.com>
2024-02-13 14:05:08 -0400

    Fix Bug

--- Lord of Dungeons/Assets/Scripts/Managers/SoundManager.cs
+++ Lord of Dungeons/Assets/Scripts/Managers/SoundManager.cs
@@ -22,7 +22,6 @@ public class SoundManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
