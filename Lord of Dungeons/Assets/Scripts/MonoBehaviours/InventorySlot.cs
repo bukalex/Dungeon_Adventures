fc5db3ccd623010e659c0196b7b8a0530e36c93a
Aleksei Erdakov <145776762+Gr8Noob179@users.noreply.github.com>
2024-02-16 16:51:40 -0500

    Revert "SceneChanger"
    This reverts commit 1dd4292952639d190c9eb7231a7b22ef98bc7e46.

--- Lord of Dungeons/Assets/Scripts/MonoBehaviours/InventorySlot.cs
+++ Lord of Dungeons/Assets/Scripts/MonoBehaviours/InventorySlot.cs
@@ -26,7 +26,7 @@ public class InventorySlot : MonoBehaviour, IDropHandler
     }
     public void OnDrop(PointerEventData eventData)
     {
-        if(transform.childCount == 01)
+        if(transform.childCount == 0)
         {
             GameObject dropped = eventData.pointerDrag;
             InventoryItem inventoryItem = dropped.GetComponent<InventoryItem>();
