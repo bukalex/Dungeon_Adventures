9c54142614256bfe4a685b7cb8704f4eac58af1e
Aleksei Gurylev <buka_lex@mail.ru>
2024-02-26 14:46:10 +0300

    Scenes renamed.

--- Lord of Dungeons/Assets/Scripts/MonoBehaviours/SceneChanger.cs
+++ Lord of Dungeons/Assets/Scripts/MonoBehaviours/SceneChanger.cs
@@ -3,11 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    [SerializeField] private int SceneToChange;
+    private int SceneToChange;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            SceneToChange = SceneManager.GetActiveScene().buildIndex + 1;
             CheckpointManager.Instance.ChangeLevel(SceneToChange);
             SceneManager.LoadScene(SceneToChange);
         }
