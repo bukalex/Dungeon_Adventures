2ab3c5f4151333ada76a86cdbd25c1a8a16b053d
bukalex <gurylevalexey@gmail.com>
2024-03-02 04:53:12 +0300

    Something.

--- Lord of Dungeons/Assets/Scripts/MonoBehaviours/SceneChanger.cs
+++ Lord of Dungeons/Assets/Scripts/MonoBehaviours/SceneChanger.cs
@@ -4,6 +4,13 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     private int SceneToChange;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
