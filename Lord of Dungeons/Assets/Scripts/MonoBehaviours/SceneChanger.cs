4e346d1dd7d6715c161b81cb5ce61348377fe39e
Gr8Noob179 <erdakov18@gmail.com>
2024-03-03 17:42:56 -0500

    Game Start Menu

--- Lord of Dungeons/Assets/Scripts/MonoBehaviours/SceneChanger.cs
+++ Lord of Dungeons/Assets/Scripts/MonoBehaviours/SceneChanger.cs
@@ -1,23 +1,55 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class SceneChanger : MonoBehaviour
 {
+    public Animator animator;
+    public Button ContinueButton;
+    public Button CreditsButton;
+    public Button CrossButton;
+    public GameObject MenuUI;
+    public GameObject CreditsUI;
     private int SceneToChange;
     private void Update()
     {
         if (Input.GetKey(KeyCode.Space))
-        {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
+
+        CreditsButton.onClick.AddListener(() => Credits());
+        CrossButton.onClick.AddListener(() => Credits());
+        ContinueButton.onClick.AddListener(() => ContinueGame());
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
-        {
-            SceneToChange = SceneManager.GetActiveScene().buildIndex + 1;
-            CheckpointManager.Instance.ChangeLevel(SceneToChange);
-            SceneManager.LoadScene(SceneToChange);
-        }
+            animator.SetTrigger("FadeOut");
     }
+    public void LoadNextScene()
+    {
+        SceneToChange = SceneManager.GetActiveScene().buildIndex + 1;
+        CheckpointManager.Instance.ChangeLevel(SceneToChange);
+        SceneManager.LoadScene(SceneToChange);
+    }
+    public void NewGame()
+    {
+
+    }
+    public void ContinueGame()
+    {
+        animator.SetTrigger("FadeOut");
+        SceneManager.LoadScene(1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void Credits()
+    {
+        MenuUI.SetActive(!MenuUI.activeSelf);
+        CreditsUI.SetActive(!MenuUI.activeSelf);
+    }
+
+
 }
