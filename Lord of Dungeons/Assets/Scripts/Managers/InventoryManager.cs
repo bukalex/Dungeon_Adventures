cb86de758744dc5973b2262db56632f7d0d729ee
Aleksei Gurylev <buka_lex@mail.ru>
2024-03-04 16:57:44 +0300

    Start scene done.

--- Lord of Dungeons/Assets/Scripts/Managers/InventoryManager.cs
+++ Lord of Dungeons/Assets/Scripts/Managers/InventoryManager.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEditor.Playables;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
--- Lord of Dungeons/Assets/Scripts/MonoBehaviours/GameStartMenu.cs
+++ Lord of Dungeons/Assets/Scripts/MonoBehaviours/GameStartMenu.cs
@@ -2,18 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class GameStartMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private string filePath = "Assets/Resources/GameData.json";
+
+    public void NewGame()
     {
-        
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    public void ChangeToNextScene()
+    public void Continue()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
