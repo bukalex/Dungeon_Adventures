810b0102422d43ca1946db2717765cc5486df0a9
Aleksei Erdakov <145776762+Gr8Noob179@users.noreply.github.com>
2024-04-05 21:02:06 -0400

    Cooking pot UI created

---
+++ Lord of Dungeons/Assets/Scripts/MonoBehaviours/PotWindow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PotWindow : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text;
+    [SerializeField] private Button addIngridientButton;
+    [SerializeField] private Button startCookingButton;
+    [SerializeField] private InventorySlot ingridientSlot;
+    [SerializeField] private InventorySlot dishSlot;
+
+    
+}
